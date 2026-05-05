f5cb36bf3e696a043d23ab1e9fa4148451b9cf81
raissaquezialopes <raissaquezia123@gmail.com>
2026-05-04 23:19:14 -0300

    feat: created and updated repositories

--- src/main/java/com/example/userservice/repositories/ClientRepository.java
+++ src/main/java/com/example/userservice/repositories/ClientRepository.java
@@ -2,8 +2,12 @@ package com.example.userservice.repositories;
 
 import com.example.userservice.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+    Optional<Client> findByClientId(String clientId);
 }
--- src/main/java/com/example/userservice/repositories/UserRepository.java
+++ src/main/java/com/example/userservice/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.userservice.repositories;
 
 import com.example.userservice.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByLoginAndClient_Id(String login, UUID clientId);
+    Optional<User> findByLoginAndClientId(String login, String clientId);
 }
